88caa0b6e924b4d6a7a755f20720c3dcd355b46c
gabriel-jolly@orange.fr <gabriel-jolly@orange.fr>
2023-02-03 09:52:19 +0100

    action research recipe and select recipe

--- actions/actions.py
+++ actions/actions.py
@@ -5,42 +5,79 @@
 # https://rasa.com/docs/rasa/custom-actions
 
 
-# This is a simple example for a custom action which utters "Hello World!"
-
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
-
 from typing import Any, Text, Dict, List
 
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
+import csv
 
 class ActionFeur(Action):
     def name(self) -> Text:
         return "action_feur"
-    
+
     def run(self, dispatcher: CollectingDispatcher,
-        tracker: Tracker,
-        domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         dispatcher.utter_message(text="Feur !")
 
         return []
+
+
+recipe_list = []
+
+class ActionResearchRecipe(Action):
+    def name(self) -> Text:
+        return "action_research_recipe"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        recipe = tracker.get_slot('recipe')
+        with open('../data/recipes.csv', 'r') as file:
+            reader = csv.DictReader(file)
+            recipe_list.clear()
+            recipe_list.append(row['Title'] for row in reader if recipe.lower() in row['Title'].lower())
+
+        if recipe_list:
+            reply = "Which recipe do you want to cook ?"
+            for i, item in enumerate(recipe_list):
+                reply += f"\n{i+1} - {item}"
+
+            dispatcher.utter_message(reply)
+        else:
+            dispatcher.utter_message(f"I could not find recipes matching with {recipe}")
+
+        return []
+
+
+class ActionSelectRecipe(Action):
+    def name(self) -> Text:
+        return "action_select_recipe"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        recipe_choice = tracker.get_slot('recipe_choice')
+        recipe_name = recipe_list[recipe_choice-1]
+        with open('../data/recipes.csv', 'r') as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                if row['Title'].lower() == recipe_name.lower():
+                    recipe = row
+                    break
+
+        if recipe:
+            # TODO
+            reply = "Which recipe do you want to cook ?"
+            for i, item in enumerate(recipe_list):
+                reply += f"\n{i+1} - {item}"
+
+            dispatcher.utter_message(reply)
+        else:
+            dispatcher.utter_message(f"I could not find recipes matching with {recipe}")
+
+        return []
+
 
 
 class ActionCook(Action):
