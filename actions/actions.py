2a392bca00b1803296f21426c9c319662577db7d
Thomas Clouet <thomas.clouet1@etu.univ-nantes.fr>
2023-02-08 08:38:25 +0100

    :art: Removed useless actions + new rules

--- actions/actions.py
+++ actions/actions.py
@@ -4,35 +4,11 @@
 # See this guide on how to implement these action:
 # https://rasa.com/docs/rasa/custom-actions
 
-
-# This is a simple example for a custom action which utters "Hello World!"
-
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
-
+import pandas as pd
 from typing import Any, Text, Dict, List
-
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.events import SlotSet
-
-import pandas as pd
 
 recipe_list=[]
 
@@ -43,6 +19,7 @@ class ActionFeur(Action):
     def run(self, dispatcher: CollectingDispatcher,
         tracker: Tracker,
         domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
         dispatcher.utter_message(text="Feur !")
 
         return []
@@ -73,7 +50,7 @@ class ActionResearchRecipe(Action):
 
         recipe = tracker.get_slot('recipe')
 
-        reader = pd.read_csv('./data/test_liste.csv')
+        reader = pd.read_csv('./csv/recipes.csv')
         reader = reader[reader['Title'].str.contains(recipe, regex=False, na=False, case=False)]
         recipe_list = reader['Title'].tolist()
 
@@ -98,11 +75,10 @@ class ActionSelectRecipe(Action):
         recipe_choice = tracker.get_slot('recipe_choice')
         recipe_list = tracker.get_slot('recipe_list')
 
-        reader = pd.read_csv('./data/test_liste.csv')
+        reader = pd.read_csv('./csv/recipes.csv')
 
         recipe_name = recipe_list[(int(recipe_choice)-1)]
 
-        #reader = pd.read_csv('./data/test_liste.csv')
         recipe = reader[reader['Title'].str.contains(recipe_name, regex=False, na=False, case=False)]
 
         if not(recipe.empty):
@@ -127,11 +103,10 @@ class ActionDisplayIngredients(Action):
         recipe_choice = tracker.get_slot('recipe_choice')
         recipe_list = tracker.get_slot('recipe_list')
 
-        reader = pd.read_csv('./data/test_liste.csv')
+        reader = pd.read_csv('./csv/recipes.csv')
 
         recipe_name = recipe_list[(int(recipe_choice)-1)]
 
-        #reader = pd.read_csv('./data/test_liste.csv')
         recipe = reader[reader['Title'].str.contains(recipe_name, regex=False, na=False, case=False)]
 
         if not(recipe.empty):
@@ -150,60 +125,5 @@ class ActionDisplayIngredients(Action):
             dispatcher.utter_message(reply)
         else:
             dispatcher.utter_message(f"I could not find recipes matching with {recipe['Title']}")
-
-        return []
-
-class ActionHello(Action):
-    def name(self) -> Text:
-        return "utter_greet"
-
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        dispatcher.utter_message(text="Bonjour ! Comment allez-vous ?")
-
-        return []
-
-class ActionByebye(Action):
-    def name(self) -> Text:
-        return "utter_goodbye"
-
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        dispatcher.utter_message(text="Ciao")
-
-        return []
-
-class ActionHappy(Action):
-    def name(self) -> Text:
-        return "utter_happy"
-
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        dispatcher.utter_message(text="I am so happy for youuuu :))")
-
-        return []
-
-class ActionCheerUp(Action):
-    def name(self) -> Text:
-        return "utter_cheer_up"
-
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        dispatcher.utter_message(text="Courage ! Tout va bien se passer !")
-
-        return []
-
-class ActionHelp(Action):
-    def name(self) -> Text:
-        return "utter_did_that_help"
-
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        dispatcher.utter_message(text="Did that help ?")
 
         return []
